0d260e22c5ebe8e51bcdbc846e17d03122186e04
romanzhukovv <zhukov.roman89@gmail.com>
2021-12-22 21:12:20 +0300

    finished project

--- PersonalQuiz/ViewControllers/ResultsViewController.swift
+++ PersonalQuiz/ViewControllers/ResultsViewController.swift
@@ -12,17 +12,30 @@ class ResultsViewController: UIViewController {
     @IBOutlet var descriptionLabel: UILabel!
     
     var answersChoosen: [Answer]!
-    // 2. Определить наиболее часто встречающийся тип животного
-    // 3. Отобразить результат в соответсвии с этим живтным
 
     override func viewDidLoad() {
         super.viewDidLoad()
         self.navigationItem.setHidesBackButton(true, animated: false)
+        
+        let result = getMostFrequentAnimal(answersChoosen)
+        animalLabel.text = "Вы - \(result.rawValue)"
+        descriptionLabel.text = "\(result.definition)"
     }
 }
 
 extension ResultsViewController {
-    private func defineDominantAnimal(_ answersChoosen: [Answer]) -> AnimalType {
-    
+    private func getMostFrequentAnimal(_ answersChoosen: [Answer]) -> AnimalType {
+        var countAnimal: [AnimalType: Int] = [:]
+        var mostFrequentAnimal: AnimalType = .cat
+        
+        for answer in answersChoosen {
+            countAnimal[answer.type] = (countAnimal[answer.type] ?? 0) + 1
+        }
+        
+        if let (animal, _) = countAnimal.max(by: { $0.1 < $1.1 }) {
+            mostFrequentAnimal = animal
+        }
+        
+        return mostFrequentAnimal
     }
 }
